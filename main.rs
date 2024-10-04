a69a1da6984e626d1fe641ce033bf2d1e6c0794c
electropaskyda228 <118922240+electropaskyda228@users.noreply.github.com>
2024-10-04 18:48:16 +0300

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,14 +1,19 @@
 use std::collections::HashMap;
 
 fn main() {
+    // Пример строки
     let s = String::from("youR string");
+
+    // Вывод результата
     print!("{}", checker(&s));
 }
 
 
 fn checker(s: &str) -> bool {
+    // Храним в HashMap'e для каждого символа факт его наличия в строке
     let mut data_check = HashMap::new();
 
+    // Проходимся по строке и проверяем: не встречали ли мы уже такой символ
     for symbol in s.to_lowercase().chars() {
         if data_check.contains_key(&symbol) {
             return false;
