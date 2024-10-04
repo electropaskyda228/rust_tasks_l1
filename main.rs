3332d2c4461d995a88d3670eb45e28c66b03696f
electropaskyda228 <118922240+electropaskyda228@users.noreply.github.com>
2024-10-04 21:48:07 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+use std::thread::JoinHandle;
+
+fn main() {
+    const n: usize = 10;
+    let mut massive: [usize; n] = [0; n];
+    for i in 0..n {
+        massive[i] = i + 2;
+    }
+
+    let mut vector: Vec<JoinHandle<()>> = Vec::new();
+    for number in massive {
+        let current_thread = thread::spawn(move || println!("{}", &(number * number)));
+        vector.push(current_thread);
+    }
+
+    for handle in vector {
+        handle.join().unwrap();
+    }
+}
